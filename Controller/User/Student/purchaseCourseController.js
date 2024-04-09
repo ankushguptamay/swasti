ebfa02d6e0513905a781a54e17e536af24ce1f5c
Ankush Gupta <ankushgupta9675@gmail.com>
2024-04-09 22:56:22 +0530

    createOrderYogaVolunteerCourse api done

--- Controller/User/Student/purchaseCourseController.js
+++ Controller/User/Student/purchaseCourseController.js
@@ -69,6 +69,72 @@ exports.createOrder = async (req, res) => {
     }
 };
 
+exports.createOrderYogaVolunteerCourse = async (req, res) => {
+    try {
+        // Validate body
+        const { error } = purchaseCourseValidation(req.body);
+        if (error) {
+            return res.status(400).send(error.details[0].message);
+        }
+        const { amount, currency, receipt, couponCode } = req.body; // receipt is id created for this order
+        const course = await Course.findOne({
+            where: {
+                courseName: "Yoga Volunteer Course"
+            }
+        });
+        if (!course) {
+            return res.status(400).send({
+                success: false,
+                message: "Yoga Volunteer Course is not present!"
+            });
+        }
+        const studentId = req.student.id;
+        // initiate payment
+        razorpayInstance.orders.create({ amount, currency, receipt },
+            (err, order) => {
+                if (!err) {
+                    Course_Student.create({
+                        courseId: course.id,
+                        userId: studentId,
+                        studentId: amount / 100,
+                        currency: currency,
+                        receipt: receipt,
+                        razorpayOrderId: order.id,
+                        status: "Created",
+                        razorpayTime: order.created_at,
+                        verify: false,
+                        couponCode: couponCode
+                    })
+                        .then(() => {
+                            res.status(201).send({
+                                success: true,
+                                message: `Order craeted successfully!`,
+                                data: order
+                            });
+                        })
+                        .catch((err) => {
+                            res.status(500).send({
+                                success: false,
+                                err: err.message
+                            });
+                        });
+                }
+                else {
+                    res.status(500).send({
+                        success: false,
+                        err: err.message
+                    });
+                }
+            })
+    }
+    catch (err) {
+        res.status(500).send({
+            success: false,
+            err: err.message
+        });
+    }
+};
+
 exports.verifyPayment = async (req, res) => {
     try {
         const orderId = req.body.razorpay_order_id;
