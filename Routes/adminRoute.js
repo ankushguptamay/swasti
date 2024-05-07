0443027548d720b1d994126195c92a766e8e7ce3
Ankush Gupta <ankushgupta9675@gmail.com>
2024-05-07 16:39:07 +0530

    email credentials

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -34,7 +34,6 @@ const uploadImage = require('../Middleware/uploadFile/image');
 const uploadPDF = require('../Middleware/uploadFile/pdf');
 
 // Admin
-admin.post("/register", register);
 admin.post("/login", login);
 admin.get("/admin", verifyAdminJWT, isAdminPresent, getAdmin);
 admin.put("/changePassword", verifyAdminJWT, isAdminPresent, changePassword);
