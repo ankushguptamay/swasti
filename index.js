0e6f1d7b7e15c1f90711773de621141021014369
Ankush Gupta <ankushgupta9675@gmail.com>
2024-07-20 15:00:16 +0530

    droping live database

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
-const admin = require('./Routes/adminRoute');
-const instructor = require('./Routes/instructorRoute');
-const student = require('./Routes/studentRoute');
+const bodyParser = require("body-parser");
+const admin = require("./Routes/adminRoute");
+const instructor = require("./Routes/instructorRoute");
+const student = require("./Routes/studentRoute");
 
-const db = require('./Models');
+const db = require("./Models");
 
-db.sequelize.sync()
-    .then(() => {
-        // console.log('Database is synced');
-    })
-    .catch((err) => {
-        // console.log(err);
-    });
+db.sequelize
+  .sync({ force: true })
+  .then(() => {
+    // console.log('Database is synced');
+  })
+  .catch((err) => {
+    // console.log(err);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/files', express.static('./Resource'));
+app.use("/files", express.static("./Resource"));
 
-app.use('/api/admin', admin);
-app.use('/api/instructor', instructor);
-app.use('/api/student', student);
+app.use("/api/admin", admin);
+app.use("/api/instructor", instructor);
+app.use("/api/student", student);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/cors', (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.send('Hello Cors World!');
+app.get("/cors", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send("Hello Cors World!");
 });
 
 PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
