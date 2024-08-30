21a836da0764c56c3d9d9e3463611341784b839a
Ankush Gupta <ankushgupta9675@gmail.com>
2024-08-30 22:23:26 +0530

    SMS text change

--- Util/sendOTPToMobileNumber.js
+++ Util/sendOTPToMobileNumber.js
@@ -5,7 +5,7 @@ exports.sendOTPToNumber = async (phoneNumber, otp) => {
     try {
         // const message = `Use the OTP ${otp} to log in to the Swasti Bharat Partners App.`;
         const message = `${otp} is your login OTP for Swasti Bharat Partners App. Do not share it with anyone. fB6cKbM6aqn - SWASBT`;
-        console.log(message)
+        // console.log(message)
         let response = await axios.post(
             `https://www.smsgatewayhub.com/api/mt/SendSMS?APIKey=${SMS_API_KEY}&senderid=${SMS_SENDER_ID}&channel=${SMS_CHANNEL}&DCS=0&flashsms=0&number=${phoneNumber}&text=${message}&route=${SMS_ROUTE_ID}&EntityId=${SMS_DLT_ENTITY_ID}&dlttemplateid=${SMS_DLT_TEMPLATE_ID}`
         );
